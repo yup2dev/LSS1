5b816c12cfd7890d88725311cb9aaa02c9408f40
Jserin <jsr3643@naver.com>
2023-07-24 07:01:50 +0900

    dmdj

--- src/main/java/org/example/service/MemberService.java
+++ src/main/java/org/example/service/MemberService.java
@@ -6,20 +6,16 @@ import org.example.repository.MemberRepository;
 
 
 public class MemberService {
-    private MemberRepository memberRepository;
-    public MemberService() {
-        memberRepository = Container.memberRepository;
-    }
 
     public boolean isLoginIdDup(String loginId) {
-        return memberRepository.isLoginIdDup(loginId);
+        return Container.memberRepository.isLoginIdDup(loginId);
     }
 
     public int join(String loginId, String loginPw, String name, String nickname, String birth) {
-        return memberRepository.join(loginId, loginPw, name, nickname, birth);
+        return Container.memberRepository.join(loginId, loginPw, name, nickname, birth);
     }
 
     public Member getMemberByLoginId(String loginId) {
-        return memberRepository.getMemberByLoginId(loginId);
+        return Container.memberRepository.getMemberByLoginId(loginId);
     }
 }
